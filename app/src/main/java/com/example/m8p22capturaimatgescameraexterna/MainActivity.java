00af149cd8dfdfe4883687c1a690bc5020d21d09
JordiCanasDAM <jcanasalvarez.cf@iesesteveterradas.cat>
2023-01-09 16:57:31 +0100

    Gallery finished

--- app/src/main/java/com/example/m8p22capturaimatgescameraexterna/MainActivity.java
+++ app/src/main/java/com/example/m8p22capturaimatgescameraexterna/MainActivity.java
@@ -1,14 +1,63 @@
 package com.example.m8p22capturaimatgescameraexterna;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.m8p22capturaimatgescameraexterna.R;
 
 public class MainActivity extends AppCompatActivity {
+    ActivityResultLauncher<Intent> someActivityResultLauncher;
+    public static int RC_PHOTO_PICKER = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        someActivityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            // There are no request codes
+                            Intent data = result.getData();
+                            Uri uri = data.getData();
+                            ImageView imageView = findViewById(R.id.img);
+                            imageView.setImageURI(uri);
+                        }
+                    }
+                });
+
+        Button galleryButton = findViewById(R.id.galleryButton);
+        galleryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openSomeActivityForResult(view);
+            }
+        });
     }
+
+    public void openSomeActivityForResult(View view) {
+        //Create Intent
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/jpg");
+        intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+        //Launch activity to get result
+        someActivityResultLauncher.launch(intent);
+    }
+
 }
