c4e038fdfcac503a28ec99e1543efc0511764d85
JordiCanasDAM <jcanasalvarez.cf@iesesteveterradas.cat>
2023-01-16 16:17:17 +0100

    Thumb finished

--- app/src/main/java/com/example/m8p22capturaimatgescameraexterna/MainActivity.java
+++ app/src/main/java/com/example/m8p22capturaimatgescameraexterna/MainActivity.java
@@ -2,13 +2,13 @@ package com.example.m8p22capturaimatgescameraexterna;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
@@ -16,37 +16,61 @@ import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.m8p22capturaimatgescameraexterna.R;
-
 public class MainActivity extends AppCompatActivity {
     ActivityResultLauncher<Intent> someActivityResultLauncher;
-    public static int RC_PHOTO_PICKER = 0;
+    ActivityResultLauncher<Intent> someActivityResultLauncher2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Metode que recupera la imatge seleccionada de la galeria i la posa al image view
         someActivityResultLauncher = registerForActivityResult(
                 new ActivityResultContracts.StartActivityForResult(),
                 new ActivityResultCallback<ActivityResult>() {
                     @Override
                     public void onActivityResult(ActivityResult result) {
                         if (result.getResultCode() == Activity.RESULT_OK) {
+                            ImageView imageView = findViewById(R.id.img);
                             // There are no request codes
                             Intent data = result.getData();
                             Uri uri = data.getData();
-                            ImageView imageView = findViewById(R.id.img);
                             imageView.setImageURI(uri);
                         }
                     }
-                });
+                }
+        );
+
+        someActivityResultLauncher2 = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            ImageView imageView = findViewById(R.id.img);
+                            Bundle extras = result.getData().getExtras();
+                            Bitmap imageBitmap = (Bitmap) extras.get("data");
+                            imageView.setImageBitmap(imageBitmap);
+
+                        }
+                    }
+                }
+        );
 
         Button galleryButton = findViewById(R.id.galleryButton);
         galleryButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 openSomeActivityForResult(view);
+            }
+        });
+
+        Button cameraButton = findViewById(R.id.cameraButton);
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openThumbCamera(view);
             }
         });
     }
@@ -58,6 +82,11 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
         //Launch activity to get result
         someActivityResultLauncher.launch(intent);
+    }
+
+    public void openThumbCamera(View view){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        someActivityResultLauncher2.launch(intent);
     }
 
 }
