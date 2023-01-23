361f2504c83ea7f4d98bf14c67fec9a2869bf843
JordiCanasDAM <jcanasalvarez.cf@iesesteveterradas.cat>
2023-01-23 16:48:53 +0100

    Thumb finished

--- app/src/main/java/com/example/m8p22capturaimatgescameraexterna/MainActivity.java
+++ app/src/main/java/com/example/m8p22capturaimatgescameraexterna/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.m8p22capturaimatgescameraexterna;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.GridView;
 import android.widget.ImageView;
 
 import androidx.activity.result.ActivityResult;
@@ -15,10 +20,19 @@ import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
-    ActivityResultLauncher<Intent> someActivityResultLauncher;
-    ActivityResultLauncher<Intent> someActivityResultLauncher2;
+    ActivityResultLauncher<Intent> galleryResult;
+    ActivityResultLauncher<Intent> cameraResult;
+    String currentPhotoPath = "";
+    Uri photoURI;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Metode que recupera la imatge seleccionada de la galeria i la posa al image view
-        someActivityResultLauncher = registerForActivityResult(
+        galleryResult = registerForActivityResult(
                 new ActivityResultContracts.StartActivityForResult(),
                 new ActivityResultCallback<ActivityResult>() {
                     @Override
@@ -42,17 +56,28 @@ public class MainActivity extends AppCompatActivity {
                 }
         );
 
-        someActivityResultLauncher2 = registerForActivityResult(
+        cameraResult = registerForActivityResult(
                 new ActivityResultContracts.StartActivityForResult(),
                 new ActivityResultCallback<ActivityResult>() {
                     @Override
                     public void onActivityResult(ActivityResult result) {
                         if (result.getResultCode() == Activity.RESULT_OK) {
+
+                            /* THUMBNAIL
                             ImageView imageView = findViewById(R.id.img);
                             Bundle extras = result.getData().getExtras();
                             Bitmap imageBitmap = (Bitmap) extras.get("data");
                             imageView.setImageBitmap(imageBitmap);
+                            */
 
+                            // FULL PICTURE
+                            Log.i("SUCCESS","Camera launched");
+                            galleryAddPic();
+                            Log.i("SUCCESS","Picture saved on gallery");
+                            setPic();
+                            Log.i("SUCCESS","Picture set on preview view");
+                        } else {
+                            Log.i("ERROR","Camera could not be initialized");
                         }
                     }
                 }
@@ -62,7 +87,7 @@ public class MainActivity extends AppCompatActivity {
         galleryButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openSomeActivityForResult(view);
+                openGallery(view);
             }
         });
 
@@ -70,23 +95,103 @@ public class MainActivity extends AppCompatActivity {
         cameraButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openThumbCamera(view);
+
+                //THUMBNAIL
+                //openThumbCamera(view);
+
+                //FULL PICTURE
+                dispatchTakePictureIntent();
+                Log.i("INFO","Image taken");
             }
         });
     }
 
-    public void openSomeActivityForResult(View view) {
+    public void openGallery(View view) {
         //Create Intent
         Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
         intent.setType("image/jpg");
         intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
         //Launch activity to get result
-        someActivityResultLauncher.launch(intent);
+        galleryResult.launch(intent);
     }
 
     public void openThumbCamera(View view){
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        someActivityResultLauncher2.launch(intent);
+        cameraResult.launch(intent);
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Create the File where the photo should go
+        File photoFile = null;
+        try {
+            photoFile = createImageFile();
+        } catch (IOException ex) {
+            // Error occurred while creating the File
+            Log.i("ERROR", "Image file could not be created");
+        }
+        // Continue only if the File was successfully created
+        if (photoFile != null) {
+            photoURI = FileProvider.getUriForFile(this,
+                    "com.example.android.fileprovider",
+                    photoFile);
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+            cameraResult.launch(takePictureIntent);
+        }
+    }
+
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(currentPhotoPath);
+        Log.i("PATH",currentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
+    }
+
+    private void setPic() {
+        ImageView imageView = findViewById(R.id.img);
+        //imageView.setImageURI(photoURI); Aixo pot reemplaçar tot el codi de sota (però es menys eficient)
+
+        // Get the dimensions of the View
+        int targetW = imageView.getWidth();
+        int targetH = imageView.getHeight();
+
+        // Get the dimensions of the bitmap
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+
+        BitmapFactory.decodeFile(currentPhotoPath, bmOptions);
+
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        // Determine how much to scale down the image
+        int scaleFactor = Math.max(1, Math.min(photoW/targetW, photoH/targetH));
+
+        // Decode the image file into a Bitmap sized to fill the View
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inPurgeable = true;
+
+        Bitmap bitmap = BitmapFactory.decodeFile(currentPhotoPath, bmOptions);
+        imageView.setImageBitmap(bitmap);
+
     }
 
 }
